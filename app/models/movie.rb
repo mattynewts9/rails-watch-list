294ea1dd86dbb38caff6d117e32a40304868bd26
mathew newton <mattynewts@gmail.com>
2024-09-26 19:38:00 +0800

    added more validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :lists
+  validates :title, :overview, :poster_url, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5 ], allow_nil: false }
 end
